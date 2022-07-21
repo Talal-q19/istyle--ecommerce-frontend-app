66eb23e72c00d781a113bb6e859209c7d7656aea
Talal Qasim <talalqasim41@gmail.com>
2022-07-21 16:01:32 +0500

    Deploying to Heroku

--- src/Home.js
+++ src/Home.js
@@ -14,13 +14,13 @@ function Home(){
           <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src={ipad} class="d-block w-100" alt="..."/>
+      <img src={ipad} height ="900px" class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item">
-      <img src={watch} class="d-block w-100" alt="..."/>
+      <img src={watch} height ="900px" class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item">
-      <img src={iphone} class="d-block w-100" alt="..."/>
+      <img src={iphone} height ="900px" class="d-block w-100" alt="..."/>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
